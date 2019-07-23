5c43ede55b14d24380f52ebad906158f98015f25
Kien Mai Ngoc <kienmn97@gmail.com>
2019-07-23 15:33:38 +0700

    Using pip installed model module instead of in-service model module

--- src/ml_models/models/Som.py
+++ src/ml_models/models/Som.py
@@ -1,8 +1,6 @@
 from src.ml_models import result
 from src.ml_models import validator
-# from sklearn.base import BaseEstimator
 from .Som_Estimator import WiEstimator
-from .som_core import CombineSomLvq
 
 SomParameters = [
     {
@@ -32,10 +30,5 @@ class SomResult(result.SuccessResult):
         self.add("accuracy", accuracy)
 
 
-class SomEstimator(CombineSomLvq):
-    def __init__(self, n_rows, n_cols):
-        super().__init__(n_rows, n_cols)
-        self._headers = ['DT', 'NPHI', 'GR', 'RHOB'] # just for test
-
-    # def add_header(self, header):
-    #     self._headers.append(header)
+class SomEstimator(WiEstimator):
+    pass
